5ce315b72c8fe49a5e2a18d79c1bed52940b2151
Ram Perel <ramperel98@gmail.com>
2024-02-08 16:39:40 +0100

    Added monday desktop message

--- src/Components/Auth/AuthModal.jsx
+++ src/Components/Auth/AuthModal.jsx
@@ -1,19 +1,28 @@
 import {Alert, Modal, Space} from "antd";
 import MicrosoftAuth from "./Microsoft.jsx";
 import GoogleAuth from "./Google.jsx";
+import {useState} from "react";
 
 export default function AuthModal({isOpen, closeModal}) {
+    const [clicked, setClicked] = useState(false);
+
     return <Modal open={isOpen}
                   footer={null}
                   onCancel={closeModal}
                   closable={true}
                   zIndex={99999}>
         <Space direction="vertical" style={{textAlign: "center", width: "100%"}}>
-            <GoogleAuth closeModal={closeModal}/>
-            <MicrosoftAuth closeModal={closeModal}/>
+            <GoogleAuth closeModal={() => {
+                setClicked(true);
+                closeModal();
+            }}/>
+            <MicrosoftAuth closeModal={() => {
+                setClicked(true);
+                closeModal();
+            }}/>
             <Alert message="Make sure to tick all the checkboxes when authorizing your account"/>
-            <Alert type="warning"
-                message="If you are using the monday.com Desktop app those buttons will not work. Please use the web version of monday.com to authorize your account."/>
+            {clicked && <Alert type="error"
+                               message="If you are using the monday.com Desktop app those buttons will not work. Please use the web version of monday.com to authorize your account."/>}
         </Space>
     </Modal>
 }
