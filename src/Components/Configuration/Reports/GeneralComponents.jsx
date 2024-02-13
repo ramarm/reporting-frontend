b70178a197486722bce489cfffb6ea76fe0dee46
ramarm <ramperel98@gmail.com>
2024-02-13 15:13:57 +0100

    Made the extra buttons of render on the left

--- src/Components/Configuration/Reports/GeneralComponents.jsx
+++ src/Components/Configuration/Reports/GeneralComponents.jsx
@@ -1,12 +1,18 @@
-import {Avatar, Space, Typography} from "antd";
+import {Avatar, Col, Row, Space, Typography} from "antd";
 
 const {Text} = Typography;
 
 export function renderOption({picture, name, extra}) {
     const nameInitials = name.split(" ").map((word) => word[0]).join("");
-    return <Space size={4}>
-        {picture ? <Avatar size={20} src={picture}/> : <Avatar size={20}>{nameInitials}</Avatar>}
-        <Text>{name}</Text>
-        {extra}
-    </Space>
+    return <Row>
+        <Col flex={"auto"}>
+            <Space size={4}>
+                {picture ? <Avatar size={20} src={picture}/> : <Avatar size={20}>{nameInitials}</Avatar>}
+                <Text>{name}</Text>
+            </Space>
+        </Col>
+        <Col>
+            {extra}
+        </Col>
+    </Row>
 }
