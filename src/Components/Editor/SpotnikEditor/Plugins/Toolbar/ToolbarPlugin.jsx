70dd542eca6e25b076264688cfeaa692adb10345
ramarm <ramperel98@gmail.com>
2024-03-25 19:51:16 +0100

    Made menu sticky

--- src/Components/Editor/SpotnikEditor/Plugins/Toolbar/ToolbarPlugin.jsx
+++ src/Components/Editor/SpotnikEditor/Plugins/Toolbar/ToolbarPlugin.jsx
@@ -4,7 +4,11 @@ import "./Toolbar.css";
 
 
 export default function ToolbarPlugin({toolbarPlugins, containerSelector}) {
-    return <Flex gap={Flex.gaps.XS} id="toolbar-container" wrap={true}>
+    return <Flex gap={Flex.gaps.XS} id="toolbar-container" wrap={true} style={{
+        position: "sticky",
+        top: 0,
+        backgroundColor: "white"
+    }}>
         {toolbarPlugins.map((plugin, index) => {
             return (React.cloneElement(plugin, {key: index, containerSelector: containerSelector}))
         })}
