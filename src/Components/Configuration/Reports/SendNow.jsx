76edb3485943f71483fbad3ab87d6a07e4f52ff1
ramarm <ramperel98@gmail.com>
2024-04-02 13:14:09 +0200

    Changed failed toast

--- src/Components/Configuration/Reports/SendNow.jsx
+++ src/Components/Configuration/Reports/SendNow.jsx
@@ -1,5 +1,5 @@
-import {Button, Toast} from "monday-ui-react-core";
-import {Send} from "monday-ui-react-core/icons";
+import {Button, IconButton, Toast, Flex, Text} from "monday-ui-react-core";
+import {CloseSmall, Send} from "monday-ui-react-core/icons";
 import {useEffect, useState} from "react";
 import {useMutation} from "@tanstack/react-query";
 import {sendReport} from "../../../Queries/reporting.js";
@@ -58,7 +58,17 @@ export default function SendNowButton({report, size}) {
         <Toast key="send-now-error-toast" className="send-now-toast" open={!!errorMessage}
                type={Toast.types.NEGATIVE}
                onClose={() => setErrorMessage("")}
-               autoHideDuration={5000}>
-            {errorMessage}
+               autoHideDuration={10000}>
+            <Flex gap={Flex.gaps.SMALL}>
+                <Text type={Text.types.TEXT2}
+                      color={Text.colors.ON_INVERTED}>{errorMessage}</Text>
+                <IconButton icon={CloseSmall}
+                            size={IconButton.sizes.SMALL}
+                            color={IconButton.colors.ON_PRIMARY_COLOR}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                setErrorMessage("")
+                            }}/>
+            </Flex>
         </Toast>]
 }
